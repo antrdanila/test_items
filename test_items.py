efe2f3f9646d0f4fbf1717aeae3a215a29b0c62e
antropov.d <antropov_danila2007@mail.ru>
2023-04-13 00:34:54 +0300

    add code and update README

--- test_items.py
+++ test_items.py
@@ -1,1 +1,10 @@
-import pytest
+from selenium.webdriver.common.by import By
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+# Пожалуйста, прочтите README
+# В README описано, как не засорить ПК и сэкономить нервы
+# В README описаны все шаги для корректной проверки
+def test_basket_button(browser):
+    browser.get(link)
+    button = browser.find_element(By.XPATH, "//*[@id='add_to_basket_form']/button")
+    assert button.text
